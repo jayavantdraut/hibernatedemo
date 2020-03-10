463172d8ec6593610212a83e55ad0c674994048e
Jayavant <jayavantdraut@gmail.com>
2020-03-10 18:13:57 +0530

    inital commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,21 @@
+import org.hibernate.Session;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        // Check database version
+        String sql = "select version()";
+
+        String result = (String) session.createNativeQuery(sql).getSingleResult();
+        System.out.println(result);
+
+        session.getTransaction().commit();
+        session.close();
+
+
+        HibernateUtil.shutdown();
+    }
+}
